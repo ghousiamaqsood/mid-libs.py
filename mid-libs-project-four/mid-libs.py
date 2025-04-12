ce2099ae76b67beb544f2d58638d2423c0b56189
ghouusiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2025-04-13 00:05:14 +0500

    Add files via upload

---
+++ mid-libs-project-four/mid-libs.py
@@ -0,0 +1,15 @@
+# User Inputs
+#Mad Lib Game By Python
+
+name = input("Enter the boy name:")
+programming_language = input("Enter a programming language (e.g: python, next js, typescript, html): ")
+
+mentor = input("Enter the mentor's name: ")
+
+# Story Created
+print("\nHere is my little story based on Mad Libs game!")
+print(f"Once upon a time, there was a boy who loved {name}.")
+print(f"He was very curious and always wanted to learn and create new things.")
+print(f"One day, he decided to learn {name} in an institute!")
+print(f"Luckily, he found a great mentor named {mentor} who was an expert in {name}.")
+print("And so, his journey continues...!")
